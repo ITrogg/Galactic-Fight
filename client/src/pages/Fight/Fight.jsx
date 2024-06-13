2ecea0ac09499b1dbf46de46a452d2fd8ed5da5e
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-13 18:02:11 +0200

    affichage dans la page

--- client/src/pages/Fight/Fight.jsx
+++ client/src/pages/Fight/Fight.jsx
@@ -1,12 +1,24 @@
 import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import CardCharacter from "../../components/CardCharacter/CardCharacter";
 import "./Fight.css";
 
+const userSample = {
+  atk: 35,
+  def: 60,
+  image:
+    "https://github.com/Miadil/starwars-api/blob/master/api/assets/Luke_Skywalker.png?raw=true",
+};
+
 function Fight() {
-  const [fighter] = useLoaderData();
-  console.info(fighter);
+  const [character] = useLoaderData();
+  const [player, setPlayer] = useState(userSample);
+  const [fighter, setFighter] = useState(character);
+  console.info(setFighter, setPlayer);
   return (
     <main className="background-page">
-      <h1>Fight page </h1>
+      <CardCharacter classCard="card-main-character" character={fighter} />
+      <CardCharacter classCard="card-main-character" character={player} />
     </main>
   );
 }
