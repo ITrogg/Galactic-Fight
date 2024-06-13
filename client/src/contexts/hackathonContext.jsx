5bc92721ec91bff1eeb4241044237520dc183a01
BastienDmr <domer.bastien@gmail.com>
2024-06-13 18:54:01 +0200

    Ajout du context pour le nom et l'avatar

--- client/src/contexts/hackathonContext.jsx
+++ client/src/contexts/hackathonContext.jsx
@@ -1,11 +1,16 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const HackatonContext = createContext();
 
 export function HackatonProvider({ children }) {
   const hello = "Wolrd";
+
+  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState("");
   return (
-    <HackatonContext.Provider value={{ hello }}>
+    <HackatonContext.Provider
+      value={{ hello, setName, name, avatar, setAvatar }}
+    >
       {children}
     </HackatonContext.Provider>
   );
