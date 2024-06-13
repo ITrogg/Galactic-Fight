1b6f550fd13fa8929c916e22a36275dcee9579c2
BastienDmr <domer.bastien@gmail.com>
2024-06-13 18:55:28 +0200

    Changement du Link dans le home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
         <div className="home-crawl-content">
           <h1 className="home-title-style">Légendes Galactiques</h1>
           <h2 className="home-title-style">Jeu de Cartes de Duel</h2>
-          <Link className="button-style" type="button" to="/planetes">
+          <Link className="button-style" type="button" to="/player">
             START WARS
           </Link>
         </div>
