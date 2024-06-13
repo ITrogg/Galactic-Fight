256e6c3ad3f4c5cd77c3f8c1a2a759a714f1a38d
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-06-13 22:37:49 +0200

    ajout de css sur la page plkanetes et fix de la logique de déblocage

--- client/src/components/planet/Planet.jsx
+++ client/src/components/planet/Planet.jsx
@@ -4,6 +4,7 @@ function Planet({ planet }) {
   return (
     <div className={`planet-${planet.id}`}>
       <img src={planet.image} alt={planet.name} />
+      <h2 className="title-style">{planet.name}</h2>
     </div>
   );
 }
