f383cdf812bc844b67e845c6c282687adc2a8bc5
BastienDmr <domer.bastien@gmail.com>
2024-06-13 14:59:52 +0200

    modif bdd + route creer en back

--- server/app/router.js
+++ server/app/router.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const client = require("../database/client");
+
 const router = express.Router();
 
 /* ************************************************************************* */
@@ -11,6 +13,12 @@ const itemActions = require("./controllers/itemActions");
 
 // Route to get a list of items
 router.get("/items", itemActions.browse);
+
+router.get("/planets", (req, res) => {
+  client.query("SELECT * FROM planets").then(([planets]) => {
+    res.status(200).json(planets);
+  });
+});
 
 // Route to get a specific item by ID
 router.get("/items/:id", itemActions.read);
