3062df813e5200aa4c21ada72879de51a0a1162e
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-06-14 00:19:47 +0200

    Création de la dataUser, et réalisation des messages d'introduction

---
+++ client/src/assets/datastory/story.js
@@ -0,0 +1,75 @@
+const storyIntro = [
+  {
+    id: "0",
+    type: "Intro",
+    long_text:
+      "Bienvenue dans l'univers épique de Légendes Galactiques: L'Épopée des Ewoks !",
+  },
+  {
+    id: "1",
+    type: "Intro",
+    long_text:
+      "Dans une galaxie lointaine, très lointaine, une nouvelle aventure vous attend. Toute la galaxies est au bord de la fin, et seule une poignée de braves peut renverser la vapeur.",
+  },
+  {
+    id: "2",
+    type: "Intro",
+    long_text:
+      "La princesse Leia Organa, figure emblématique de l'Alliance Rebelle, a été capturée par Dark Vador.  ",
+  },
+  {
+    id: "3",
+    type: "Intro",
+    long_text:
+      "Votre mission : la sauver et restaurer l'espoir dans la galaxie. Avant de partir au combat, il vous faut choisir votre personnage.",
+  },
+  {
+    id: "4",
+    type: "Intro",
+    long_text:
+      "Ces personnages on une statitstique d'attaque, de défense, et de point de vie. Suite à chaque combat vous gagnerez en compétences.",
+  },
+  {
+    id: "5",
+    type: "Intro",
+    long_text:
+      "Le destin de la galaxie repose entre vos mains. L'univers de Star Wars vous attend. Serez-vous à la hauteur ?",
+  },
+  {
+    id: "6",
+    type: "Carte",
+    long_text:
+      "Voici une carte de la galaxies, la princesse Leia Organa, est retenue sur une planète. À toi de trouver la princesse!",
+  },
+  {
+    id: "7",
+    type: "Planete",
+    long_text:
+      "Ici vous retrouvez plusieurs personnages à combattre, cliquez sur l'un d'eux pour l'afronter.",
+  },
+  {
+    id: "8",
+    type: "Lose",
+    long_text:
+      "Malheureusement, la force n'était pas au rendez-vous.Mais ne perdez pas espoir ! Chaque défaite est une occasion d'apprendre et de s'améliorer. Alors, debout et trouvé moi cette princesse.",
+  },
+  {
+    id: "9",
+    type: "Win",
+    long_text:
+      "Grâce à votre courage, votre stratégie et votre détermination, vous avez triomphé. ",
+  },
+  {
+    id: "10",
+    type: "Win",
+    long_text:
+      "Votre bravoure a restauré l'espoir et la paix dans la galaxie. La princesse Leia est désormais libre, votre victoire est une inspiration pour toutes la galaxie.",
+  },
+  {
+    id: "",
+    type: "",
+    long_text: "",
+  },
+];
+
+export default storyIntro;
--- client/src/pages/PlayerForm/PlayerForm.jsx
+++ client/src/pages/PlayerForm/PlayerForm.jsx
@@ -1,49 +1,112 @@
 import "./PlayerForm.css";
+import { useState } from "react";
+import ewok from "../../assets/images/ewok.png";
 
-function PlayerForm(setName) {
-  const handleSubmit = () => {};
+const messages = [
+  {
+    id: "0",
+    long_text:
+      "Bienvenue dans l'univers épique de Légendes Galactiques: L'Épopée des Ewoks !",
+  },
+  {
+    id: "1",
+    long_text:
+      "Dans une galaxie lointaine, très lointaine, une nouvelle aventure vous attend. Toute la galaxie est au bord de la fin, et seule une poignée de braves peut renverser la vapeur.",
+  },
+  {
+    id: "2",
+    long_text:
+      "La princesse Leia Organa, figure emblématique de l'Alliance Rebelle, a été capturée par Dark Vador.",
+  },
+  {
+    id: "3",
+    long_text:
+      "Votre mission : la sauver et restaurer l'espoir dans la galaxie. Avant de partir au combat, il vous faut choisir votre personnage.",
+  },
+  {
+    id: "4",
+    long_text:
+      "Ces personnages ont une statistique d'attaque, de défense, et de points de vie. Suite à chaque combat, vous gagnerez en compétences.",
+  },
+  {
+    id: "5",
+    long_text:
+      "Le destin de la galaxie repose entre vos mains. L'univers de Star Wars vous attend. Serez-vous à la hauteur ?",
+  },
+];
+
+function PlayerForm({ setName }) {
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+
+  const handleNextMessage = () => {
+    if (currentMessageIndex < messages.length - 1) {
+      setCurrentMessageIndex(currentMessageIndex + 1);
+    } else {
+      setCurrentMessageIndex(-1);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <section className="background-page">
-      <div className="page-container">
-        <div className="form-container">
-          <form>
-            <label htmlFor="avatar" className="avatar-label">
-              Choisie ton avatar
-            </label>
-            <div className="avatar-selection">
-              <img
-                src="https://github.com/Miadil/starwars-api/blob/master/api/assets/Luke_Skywalker.png?raw=true"
-                alt=""
-              />
-              <img
-                src="https://github.com/Miadil/starwars-api/blob/master/api/assets/Luke_Skywalker.png?raw=true"
-                alt=""
-              />
-              <img
-                src="https://github.com/Miadil/starwars-api/blob/master/api/assets/Luke_Skywalker.png?raw=true"
-                alt=""
-              />
+      {currentMessageIndex >= 0 ? (
+        <div className="page-container">
+          <div className="box box-position-story">
+            <div className="text-style box-story1">
+              {messages[currentMessageIndex].long_text}
             </div>
-            <label htmlFor="name" className="name-label">
-              Quel est ton nom ?
-              <input
-                type="text"
-                onChange={(e) => setName(e.target.value)}
-                required
-                className="input-form"
-              />
-            </label>
-            <button
-              type="submit"
-              onClick={handleSubmit}
-              className="button-style button-form"
-            >
-              valider
-            </button>
-          </form>
+            <div className="box-story2">
+              <button
+                onClick={handleNextMessage}
+                className="button-style button-story"
+                type="button"
+              >
+                Skip
+              </button>
+              <img src={ewok} alt="Personnage Ewok" />
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="page-container">
+          <div className="form-container">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="avatar" className="avatar-label">
+                Choisie ton avatar
+              </label>
+              <div className="avatar-selection">
+                <img
+                  src="https://github.com/Miadil/starwars-api/blob/master/api/assets/Luke_Skywalker.png?raw=true"
+                  alt="Avatar 1"
+                />
+                <img
+                  src="https://github.com/Miadil/starwars-api/blob/master/api/assets/Luke_Skywalker.png?raw=true"
+                  alt="Avatar 2"
+                />
+                <img
+                  src="https://github.com/Miadil/starwars-api/blob/master/api/assets/Luke_Skywalker.png?raw=true"
+                  alt="Avatar 3"
+                />
+              </div>
+              <label htmlFor="name" className="name-label">
+                Quel est ton nom ?
+                <input
+                  type="text"
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                  className="input-form"
+                />
+              </label>
+              <button type="submit" className="button-style button-form">
+                valider
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </section>
   );
 }
