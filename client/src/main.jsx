83980cf3e4f23f985cdfc17a71e212c00fa9f24a
BastienDmr <domer.bastien@gmail.com>
2024-06-13 15:03:25 +0200

    Ajout du loader

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { HackatonProvider } from "./contexts/hackathonContext";
 
 import App from "./App";
+import Planets from "./pages/Planets/Planets";
 
 const router = createBrowserRouter([
   {
@@ -12,8 +14,12 @@ const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "/toto",
-    element: <App />,
+    path: "/planets",
+    element: <Planets />,
+    loader: () =>
+      axios
+        .get(`${import.meta.env.VITE_API_URL}/api/planets`)
+        .then((res) => res.data),
   },
   {
     path: "/tata",
