4a65f3b2bddebae8aabe58c398328eda47fe5a76
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-14 13:20:36 +0200

    ça avance bien

--- client/src/pages/EndFight/EndFight.jsx
+++ client/src/pages/EndFight/EndFight.jsx
@@ -17,26 +17,39 @@ function EndFight() {
       setPlayerstat("def", player.def + 5);
     }
   };
+
   return (
     <main className="background-page">
       {status === "victoire" ? (
         <div className="end-page-victory">
           <CardCharacter classCard="card-battle-character" character={player} />
-          <section className="choice-stat">
-            <Link
-              className="button-style"
-              to="/planetes"
-              onClick={() => handleVictories("atk")}
-            >
-              Attaque
-            </Link>
-            <Link
-              className="button-style"
-              to="/planetes"
-              onClick={() => handleVictories("def")}
-            >
-              Defense
-            </Link>
+          <section className="box choice-stat">
+            <h1 className="title-style">vous avez gagner ce combat </h1>
+            <img
+              className="gif-victory"
+              src="https://i.giphy.com/3FQ9mRcb94aogeTvmj.webp"
+              alt="gif ewok"
+            />
+            <p className="text-style">
+              preparez vous pour le prochain combat. <br /> choisissez la
+              statistique que vous voulez augmenter.
+            </p>
+            <nav className="buttons-set-stat">
+              <Link
+                className="button-style"
+                to="/planetes"
+                onClick={() => handleVictories("atk")}
+              >
+                Attaque
+              </Link>
+              <Link
+                className="button-style"
+                to="/planetes"
+                onClick={() => handleVictories("def")}
+              >
+                Defense
+              </Link>
+            </nav>
           </section>
         </div>
       ) : (
@@ -45,9 +58,19 @@ function EndFight() {
             classCard="card-battle-character"
             character={character}
           />
-          <Link className="button-style" to="/planetes">
-            retour
-          </Link>
+          <section className="box choice-stat">
+            <h1 className="title-style">vous avez perdu ce combat </h1>
+            <p className="text-style">
+              malheureusement, la force n'était pas au rendez-vous. mais ne
+              perdez pas espoir ! chaque défaite est une occasion d'apprendre et
+              de s'améliorer. alors, debout et retournez au combat.
+            </p>
+            <nav className="buttons-set-stat">
+              <Link className="button-style" to="/planetes">
+                retour
+              </Link>
+            </nav>
+          </section>
         </div>
       )}
     </main>
