439811457254ae2abaefeffeca324d345a29bd9d
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-06-14 04:43:54 +0200

    creation page fin de combat

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,7 @@ import Planets from "./pages/Planets/Planets";
 import SelectFight from "./pages/selectFight/SelectFight";
 import PlayerForm from "./pages/PlayerForm/PlayerForm";
 import Fight from "./pages/Fight/Fight";
-
+import EndFight from "./pages/EndFight/EndFight";
 
 const router = createBrowserRouter([
   {
@@ -50,6 +50,10 @@ const router = createBrowserRouter([
       );
       return res.data;
     },
+  },
+  {
+    path: "/adversaire/:id/:status",
+    element: <EndFight />,
   },
 ]);
 
---
+++ client/src/pages/EndFight/EndFight.jsx
@@ -0,0 +1,11 @@
+import "./EndFight.css";
+
+function EndFight() {
+  return (
+    <main className="background-page">
+      <h1 className="title-style">oui </h1>;
+    </main>
+  );
+}
+
+export default EndFight;
