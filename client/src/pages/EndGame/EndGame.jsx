097c6c01f13f4d20115218f267b3c1495e479d8d
BastienDmr <domer.bastien@gmail.com>
2024-06-14 12:13:06 +0200

    Ajout du texte de fin

--- client/src/pages/EndGame/EndGame.jsx
+++ client/src/pages/EndGame/EndGame.jsx
@@ -1,11 +1,50 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import ewok from "../../assets/images/ewok.png";
+import "../../App.css";
 
 function EndGame() {
   const { id } = useParams();
 
   return (
     <main className="background-page">
-      {id === "4" ? <div>Fin dark vador</div> : <div>Fin leaia</div>}EndGame
+      {id === "4" ? (
+        <section className="display-intro">
+          <div className="box box-position-story ">
+            <div className="text-style box-story1">
+              <p>
+                votre bravoure a restauré l'espoir et la paix dans la galaxie.
+                la princesse leia est désormais libre, votre victoire est une
+                inspiration pour toute la galaxie.
+              </p>
+            </div>
+            <div className="box-story2">
+              <Link to="/" type="button" className="button-style button-story">
+                fin
+              </Link>
+              <img src={ewok} alt="Personnage Ewok" />
+            </div>
+          </div>
+        </section>
+      ) : (
+        <section className="display-intro">
+          <div className="box box-position-story ">
+            <div className="text-style box-story1">
+              <p>
+                grâce à votre courage, votre stratégie et votre détermination,
+                vous avez triomphé contre la princesse leia. vous pouvez enfin
+                vivre votre belle histoire d'amour au côté de dark vador.
+                <br /> vadou pour les intimes.
+              </p>
+            </div>
+            <div className="box-story2">
+              <Link to="/" type="button" className="button-style button-story">
+                fin
+              </Link>
+              <img src={ewok} alt="Personnage Ewok" />
+            </div>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
